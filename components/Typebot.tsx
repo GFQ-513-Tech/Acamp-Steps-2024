1e1c7fb837366f5e0a6d67f9ea0df5233dd92b4a
João Lucas Pereira <j.lucaspereira@usp.br>
2024-01-26 23:32:15 -0300

    change typebot component

--- components/Typebot.tsx
+++ components/Typebot.tsx
@@ -7,7 +7,21 @@ const Bubble = dynamic(() => import('@typebot.io/react').then((module) => module
 const TypebotComponent = () => {
   return (
     <BotWrapper>
-      <Bubble typebot="1-apresenta-o-r7pb3yk" apiHost="https://viewer.gfq513.com.br" />;
+      <Bubble
+        typebot="0-inicio-dflob8d"
+        apiHost="https://viewer.gfq513.com.br"
+        previewMessage={{
+          message: 'Bora acampar?! 😃',
+          autoShowDelay: 5000,
+          avatarUrl:
+            'https://s3.typebot.io/public/workspaces/clrf92v5j000z6nxkv54ggijh/typebots/clrm72g6j0001zrkdyxgzd3za/hostAvatar?v=1705763403125',
+        }}
+        theme={{
+          button: { backgroundColor: '#1f432e', size: 'medium' },
+          previewMessage: { backgroundColor: '#fff', textColor: '#000000' },
+        }}
+      />
+      ;
     </BotWrapper>
   );
 };
